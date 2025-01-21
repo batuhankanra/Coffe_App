88295078e760086ab35fd4f4c2be97eb7b335f26
batuhan <batuhan_kanra_60@hotmail.com>
2025-01-21 14:45:22 +0300

    Enum routes added,#1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import config from "./src/config/index.js";
 import Database from "./src/db/Database.js";
 import morgan from "morgan";
+import routes from "./src/routes/index.routes.js";
 
 const app =express()
 
@@ -10,6 +11,9 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 Database.connect({Connect:config.DB_URL})
+
+
+app.use('/api',routes)
 
 app.listen(config.PORT,()=>{
     console.log('server is running...',config.PORT)
